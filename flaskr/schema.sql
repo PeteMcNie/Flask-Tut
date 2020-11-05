5b6113f2dd095373a5e2187082216b096d3ea676
petemcnie <petemcnie0@gmail.com>
2020-11-06 11:09:41 +1300

    Debug sql code

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -7,11 +7,11 @@ CREATE TABLE user (
     password TEXT NOT NULL
 );
 
-CREAT TABLE post (
+CREATE TABLE post (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     author_id INTEGER NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     title TEXT NOT NULL,
     body TEXT NOT NULL,
-    FOREGIN KEY (author_id) REFERENCES user (id)
+    FOREIGN KEY (author_id) REFERENCES user (id)
 );
