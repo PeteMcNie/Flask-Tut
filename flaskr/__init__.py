212966c178403bed248fd22c28d05f62d7d58294
petemcnie <petemcnie0@gmail.com>
2020-11-09 11:42:32 +1300

    Add blog blueprint to factory function

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -35,4 +35,8 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index') # Unlike the auth blueprint, the blog blueprint does not have a url_prefix. So the index view will be at /, the create view at /create, and so on. The blog is the main feature of Flaskr, so it makes sense that the blog index will be the main index.
+                                    # However, the endpoint for the index view defined below will be blog.index. Some of the authentication views referred to a plain index endpoint. app.add_url_rule() associates the endpoint name 'index' with the / url so that url_for('index') or url_for('blog.index') will both work, generating the same / URL either way.
     return app
